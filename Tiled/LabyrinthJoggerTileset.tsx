52be0422d0e9e0e2f235578d8885924bfe47d6aa
Sean LaPlante <laplante.sean@gmail.com>
2020-08-02 13:30:51 -0400

    some art and stuff

---
+++ Tiled/LabyrinthJoggerTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="LabyrinthJoggerTileset" tilewidth="8" tileheight="8" tilecount="140" columns="14">
+ <image source="colored_tilemap_packed.png" trans="222323" width="112" height="80"/>
+</tileset>
