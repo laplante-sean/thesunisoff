57fd5c7c3f50a9736ec89c117933c8c669e973fb
Sean LaPlante <laplante.sean@gmail.com>
2020-08-02 21:03:07 -0400

    Added player, new animations, movement, and Y-Sort

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,16 +1,54 @@
 extends KinematicBody2D
+class_name Player
+
+export(int) var ACCELERATION = 200
+export(int) var MAX_SPEED = 25
+export(int) var FRICTION = 175
+
+enum {
+	MOVE,
+	ATTACK
+}
+
+var state = MOVE
+var velocity = Vector2.ZERO
+
+var MainInstances = ResourceLoader.load("res://maininstances/MainInstances.tres")
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	animationTree.active = true
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ATTACK:
+			pass  # TODO
+
+
+func move_state(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+
+	move()
+
+
+func move():
+	velocity = move_and_slide(velocity)
